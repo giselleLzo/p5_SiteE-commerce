bdc90c8d24442cb05daf43437b5afac99d99ee7a
Giselle <gisellelsz@gmail.com>
2021-06-14 20:12:40 +0200

    Add comments, catch changes

--- front-end/js/produit.js
+++ front-end/js/produit.js
@@ -1,9 +1,9 @@
 /** Gère les interactions de la page Produit */
 
 let params = (new URL(document.location)).searchParams;
-let id = params.get("id"); // Récupérer le id du produit 
+let id = params.get("id"); //Récupérer le id du produit 
 
-// Récupérer les éléments de chaque produit 
+//Récupérer les informations de chaque produit 
 function productPage(product) {
     document.getElementById('imgCamera').src = product.imageUrl
     document.getElementById('nameCamera').textContent = product.name
@@ -11,7 +11,7 @@ function productPage(product) {
     document.getElementById('priceCamera').textContent = `${product.price / 100}.00 €`
     selectLenses = document.querySelector("select");
 
-    // Création des éléments du menu déroulant 
+    //Création des éléments du menu déroulant 
     for (i = 0; i < product.lenses.length; i++) {
       let option = document.createElement("option");
       option.textContent = product.lenses[i];
@@ -19,41 +19,40 @@ function productPage(product) {
     }
 }
 
-// Appelle l'api de produits
+//Appelle l'api de produits
 function getCamera(id) {
     fetch(apiUrl + "/api/cameras/" + id)
       .then(response => response.json())
       .then(product => {
         productPage(product);
         
-      // Ecouter les clics sur le bouton addToBasket
+      //Ecouter les clics sur le bouton addToBasket
       let addItemToBasket = document.querySelector("#addToBasket");
       addItemToBasket.addEventListener("click", function () {
         
         addToBasket(product);
-        alert("Ajouté au panier");
+        alert("Ajouté au panier"); //Alert de l'ajoute au panier d'un produit
       
       });
     })
     .catch(function() {
-        console.log('Erreur de chargement');
     });
 }
 
 function addToBasket(product) {
     
-    // Création du panier dans le localStorage s'il n'existe pas déjà
+    //Création du panier dans le localStorage s'il n'existe pas déjà
     if (typeof localStorage.getItem("basket") !== "string") {
         let basket = [];
         localStorage.setItem("basket", JSON.stringify(basket));
     }
 
-    // Récupérer les informations de la caméra
+    //Récupérer les informations du lenses de la caméra
     product.selectedLense = document.querySelector("option:checked").innerText;
     delete product.lenses;
-    // Création d'une variable pour manipuler le panier
+    //Création d'une variable pour manipuler le panier
     let basket = JSON.parse(localStorage.getItem("basket"));
-    // Vérification que l'item n'existe pas déjà dans le panier
+    //Vérification que l'item n'existe pas déjà dans le panier
     let isThisItemExist = false;
     let existingItem;
     for (let i = 0; i < basket.length; i++) {
@@ -63,13 +62,11 @@ function addToBasket(product) {
       }
     }
     
-    // Ajouter la caméra au panier
-    if (isThisItemExist === false) {
+    //Ajouter la caméra au panier
+    if (!isThisItemExist) {
       basket.push(product);
+    } 
       localStorage.setItem("basket", JSON.stringify(basket));
-    } else {
-      localStorage.setItem("basket", JSON.stringify(basket));
-    }
   }
 
     
